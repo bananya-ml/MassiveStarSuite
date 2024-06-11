0142ae331d2a05f5ee9c1766bea868761a25eb40
Ananya Bhatnagar <bhatnagarananya64@gmail.com>
2024-06-12 01:17:13 +0530

    add clf model and train script

---
+++ src/model.py
@@ -0,0 +1,63 @@
+from torch.nn import functional as F
+import torch.autograd as autograd
+from torch import nn
+import torch
+
+class StellarNet(nn.Module):
+    def __init__(self, input_channels):
+        super(StellarNet, self).__init__()
+        
+       # spectrum convolutional input
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=4, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(in_channels=4, out_channels=16, kernel_size=5, padding=1)
+        self.conv3 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=8, padding=1)
+        self.pool = nn.MaxPool1d(kernel_size=2)
+        
+        pool_output_shape = self._compute_out_size((1,input_channels), 
+                                             nn.Sequential(self.conv1, 
+                                                           self.pool,
+                                                           self.conv2,
+                                                           self.pool,
+                                                           self.conv3,
+                                                           self.pool))
+
+        self.fc01 = nn.Linear(pool_output_shape[0]*pool_output_shape[1], 128)
+        self.fc02 = nn.Linear(128, 64)
+        
+        # context input
+        self.fc11 = nn.Linear(2, 16)
+        self.ln1 = nn.LayerNorm(16)
+        self.fc12 = nn.Linear(16, 32)
+        
+        # combine outputs
+        self.fc = nn.Linear(64+32, 64)
+        self.output = nn.Linear(64, 1)
+
+
+    def forward(self, x, y):
+
+        # forward pass for spectrum
+        x = self.pool(F.leaky_relu(self.conv1(x)))
+        x = self.pool(F.leaky_relu(self.conv2(x)))
+        x = self.pool(F.leaky_relu(self.conv3(x)))
+        x = x.view(x.size(0), -1)
+        x = F.leaky_relu(self.fc01(x))
+        x = F.leaky_relu(self.fc02(x))
+        
+        # forward pass for context
+        y = self.ln1(F.leaky_relu(self.fc11(y)))
+        y = F.leaky_relu(self.fc12(y))
+
+        out = torch.cat((x, y), 1)
+        out = F.leaky_relu(self.fc(out))
+        out = self.output(out)
+        
+        return out
+    
+    def _compute_out_size(self, in_size, mod):
+        """
+        Compute output size of Module `mod` given an input with size `in_size`.
+        """
+        
+        f = mod.forward(autograd.Variable(torch.Tensor(1, *in_size)))
+        return f.size()[1:]
---
+++ src/train.py
@@ -0,0 +1,239 @@
+import os
+import torch
+from torch import nn
+from torch.optim.lr_scheduler import OneCycleLR
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+from model import StellarNet
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.utils.class_weight import compute_class_weight
+
+
+def plot(training_losses, validation_losses, accuracy_scores, filename, save_fig=True):
+    
+    filename ='../results/training_curves.png'
+
+    _, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 14))
+    
+    ax1.plot(accuracy_scores, label='Validation Accuracy')
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Accuracy')
+    ax1.set_title('Accuracy')
+    ax1.legend()
+
+    ax1.annotate(f'{accuracy_scores[0]:.2f}', (0, accuracy_scores[0]), textcoords="offset points", xytext=(0,10), ha='center')
+    ax1.annotate(f'{accuracy_scores[-1]:.2f}', (len(accuracy_scores)-1, accuracy_scores[-1]), textcoords="offset points", xytext=(0,10), ha='center')
+
+    ax2.plot(training_losses, label='Training Loss')
+    ax2.plot(validation_losses, label='Validation Loss')
+    ax2.set_xlabel('Epochs')
+    ax2.set_ylabel('Loss')
+    ax2.set_title('Training and Validation Loss')
+    ax2.legend()
+
+    ax2.annotate(f'{training_losses[0]:.2f}', (0, training_losses[0]), textcoords="offset points", xytext=(0,10), ha='center')
+    ax2.annotate(f'{training_losses[-1]:.2f}', (len(training_losses)-1, training_losses[-1]), textcoords="offset points", xytext=(0,10), ha='center')
+    ax2.annotate(f'{validation_losses[0]:.2f}', (0, validation_losses[0]), textcoords="offset points", xytext=(0,10), ha='center')
+    ax2.annotate(f'{validation_losses[-1]:.2f}', (len(validation_losses)-1, validation_losses[-1]), textcoords="offset points", xytext=(0,10), ha='center')
+
+    plt.show()
+
+    if save_fig:
+        plt.savefig(filename)
+    
+def _extend_curves(curves):
+    '''
+    function to extended loss curves to the same length
+    '''
+    max_length = max(len(curve) for curve in curves)
+    
+    extended_curves = []
+    for curve in curves:
+        last_value = curve[-1]
+        extended_curve = curve + [last_value] * (max_length - len(curve))
+        extended_curves.append(extended_curve)
+    
+    return np.array(extended_curves)
+
+def _init_weights(m):
+    if isinstance(m, nn.Conv1d) or isinstance(m, nn.Linear):
+        nn.init.kaiming_uniform_(m.weight, nonlinearity='relu')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+
+def train_model(X, y, spectrum_width):
+
+    def fit(model, x_train, y_train, x_val=None, y_val=None, prt_steps = 1, verbose=True):
+    
+        model.apply(_init_weights)
+
+        # hyperparameters
+        epochs = 50
+        learning_rate = 1e-4
+        batch_size = 64
+        device = 'cuda'
+
+        # early stopping
+        patience = 10
+        best_val_loss = float('inf')
+        patience_counter = 0
+        
+        class_weights = torch.tensor(compute_class_weight(class_weight='balanced',classes=np.unique(y_train), y=y_train.numpy())).to(device)
+
+        # model components
+        criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights[1])
+        optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
+        
+        # move everything to gpu
+        model.to(device)
+        x_train = x_train.to(device)
+        y_train = y_train.unsqueeze(1).to(device)
+        x_val = x_val.to(device)
+        y_val = y_val.unsqueeze(1).to(device)
+
+        # metrics
+        training_losses, validation_losses = [], []
+        accuracy = []
+        
+        # lr cycling
+        max_lr = 1e-3
+        steps_per_epoch = (len(x_train) + batch_size - 1) // batch_size
+        scheduler = OneCycleLR(optimizer, max_lr=max_lr, steps_per_epoch=steps_per_epoch, epochs=epochs)
+
+        batch_start = torch.arange(0, len(x_train), batch_size)
+
+        t = tqdm(range(epochs), leave=True, dynamic_ncols=True, desc="Epochs")
+
+        for epoch in t:
+            
+            model.train()
+            running_loss = 0
+
+            for start in batch_start:
+                
+                x_spectra = x_train[start:start+batch_size,:-2]
+                x_context = x_train[start:start+batch_size,-2:]
+            
+                y_batch = y_train[start:start+batch_size]
+                
+                output = model(x_spectra.unsqueeze(1), x_context)
+                loss = criterion(output, y_batch)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+
+                running_loss += loss.item() * x_spectra.size(0)
+
+            train_loss = running_loss / len(x_train)
+            training_losses.append(train_loss)
+            if verbose and (epoch+1) % prt_steps == 0:
+                t.set_postfix({'Train Loss':train_loss})
+
+            if x_val is not None and y_val is not None:
+                model.eval()
+                preds, labels = [], []
+
+                with torch.no_grad():
+                    
+                    x_spectra = x_val[:,:-2]
+                    x_context = x_val[:,-2:]
+                    output = model(x_spectra.unsqueeze(1), x_context)
+                    loss = criterion(output, y_val)
+
+                    probs = torch.sigmoid(output)
+                    pred = torch.round(probs).cpu().numpy().astype(float)  # pred: [batch_size]
+                    
+                    preds.extend(pred)
+                    labels.extend(y_val.cpu().numpy())
+                    
+                    val_loss = loss.item()
+                
+                epoch_acc = accuracy_score(labels, preds)
+                validation_losses.append(val_loss)
+                accuracy.append(epoch_acc)
+                if verbose and (epoch+1) % prt_steps == 0:
+                    t.set_postfix({'Train loss': train_loss, 'Val Loss': val_loss, 'Accuracy': epoch_acc})
+
+                # Early stopping
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    patience_counter = 0
+                else:
+                    patience_counter += 1
+
+                if patience_counter >= patience:
+                    t.set_postfix_str(f'Early stopping triggered at epoch {epoch+1}')
+                    break
+
+        return model, training_losses, validation_losses, accuracy
+
+    kfold = StratifiedKFold(n_splits=10)
+    training_losses_foldx, validation_losses_foldx, accuracy_scores_foldx, models = [], [], [], []
+
+    model = StellarNet(input_channels=spectrum_width)
+
+    for fold, (train_idx, val_idx) in enumerate(kfold.split(X, y)):
+        
+        print(f"\nFitting fold {fold+1}")
+
+        model, tr_loss, val_loss, acc = fit(model, X[train_idx], y[train_idx], X[val_idx], y[val_idx], prt_steps=10, verbose=True)
+        models.append(model)
+        training_losses_foldx.append(tr_loss)
+        validation_losses_foldx.append(val_loss)
+        accuracy_scores_foldx.append(acc)
+
+    training_losses = np.mean(_extend_curves(training_losses_foldx), axis=0)
+    validation_losses = np.mean(_extend_curves(validation_losses_foldx), axis=0)
+    accuracy_scores = np.mean(_extend_curves(accuracy_scores_foldx), axis=0)
+
+    plot(training_losses, validation_losses, accuracy_scores, save_fig=True)
+
+    return models
+
+def prepare_data(data_dir):
+    
+    data = pd.read_parquet(data_dir)
+    df = data.drop(columns = ['teff_gspphot', 'logg_gspphot', 'mh_gspphot', 'spectraltype_esphs'])
+
+    # create context
+    df['min'] = df['flux'].apply(np.min)
+    df['max'] = df['flux'].apply(np.max)
+
+    spectrum_width = len(df['flux'][0])
+
+    spectra = df['flux'].to_numpy()
+    context = df[['max','min']].to_numpy()
+
+    # L2 normalization
+    spectra = torch.from_numpy(np.array([spectrum / np.linalg.norm(spectrum, keepdims=True) for spectrum in spectra])).float()
+    context = torch.tensor(context)
+
+    X = torch.cat([spectra,context],1)
+    y = df['Cat'].to_numpy()
+
+    # encode categories to int
+    y = torch.from_numpy(np.where(y == 'M', 1, np.where(y == 'LM', 0, y)).astype(float))
+
+    return X, y, spectrum_width
+
+def main():
+
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    data_dir = 'data/train.parquet'
+
+    X, y, spectrum_width = prepare_data(data_dir)
+    print(type(X))
+    print("Data loaded!")
+    models = train_model(X, y, spectrum_width)
+
+    # save trained model
+    torch.save(models,'../trained_models/StellarNet.pth')
+
+if __name__ == "__main__":
+    main()
